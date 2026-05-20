d772902a8f47f6c851afe2b8b592fc2f58f78962
Abdulla <abdullainomboyev@gmail.com>
2026-05-20 16:20:58 +0500

    Remove i18n, single language uz

--- config/urls.py
+++ config/urls.py
@@ -2,18 +2,12 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
-from django.conf.urls.i18n import i18n_patterns
 
 admin.site.site_header = "NamDU Xalqaro Bo'lim"
 admin.site.site_title = "NamDU Admin"
 admin.site.index_title = "Boshqaruv paneli"
 
 urlpatterns = [
-    path('i18n/', include('django.conf.urls.i18n')),
-    path('rosetta/', include('rosetta.urls')),
-]
-
-urlpatterns += i18n_patterns(
     path('admin/', admin.site.urls),
     path('ckeditor/', include('ckeditor_uploader.urls')),
     path('', include('apps.core.urls')),
@@ -28,8 +22,7 @@ urlpatterns += i18n_patterns(
     path('conferences/', include('apps.conferences.urls')),
     path('community/', include('apps.community.urls')),
     path('reports/', include('apps.reports.urls')),
-    prefix_default_language=False,
-)
+]
 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
